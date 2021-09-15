382adf0a2bbe933297000a604058a873ed205989
Gian Carlos <gpaucarcortez@gmail.com>
2021-09-15 00:07:18 -0500

    Iniciando BMI

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI App"),
+      ),
+    );
+  }
+}
+
